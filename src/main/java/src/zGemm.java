8b5c869a405f45d4b06705d6ee1f8afd4d518f4c
zuoanke <228823384@qq.com>
2022-12-03 03:57:58 -0600

    add matrix split
    Signed-off-by: zuoanke <228823384@qq.com>

--- src/main/java/src/zGemm.java
+++ src/main/java/src/zGemm.java
@@ -5,10 +5,7 @@ import cn.hutool.core.util.StrUtil;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class zGemm {
     public List<List<Long>> readMat(String strPath) {
@@ -163,46 +160,145 @@ public class zGemm {
         return ans;
     }
 
-    public static void main(String[] args) {
-        String filePath1 = "/Users/zuoankembp/Desktop/CSE8377/zGemm/matrix1.csv";
-        String filePath2 = "/Users/zuoankembp/Desktop/CSE8377/zGemm/matrix2.csv";
-        zGemm zg = new zGemm();
-        //get matrix
-        List<List<Long>> mat1 = zg.readMat(filePath1);
-        List<List<Long>> mat2 = zg.readMat(filePath2);
+    public class SplitedMat {
+        int[] pos;
+        List<List<Long>> mat;
+        SplitedMat(int row, int col, List<List<Long>> mat) {
+            pos = new int[2];
+            this.pos[0] = row;
+            this.pos[1] = col;
+            this.mat = mat;
+        }
+    }
 
-        long cStart = System.currentTimeMillis();
-        //matrix mul
-        List<List<Long>> newMat = zg.matMuli(mat1, mat2);
-        long cEnd = System.currentTimeMillis();
+    public List<SplitedMat> splitMatrix(List<List<Long>> matrix) {
+        List<SplitedMat> ans = new ArrayList<>();
+        int colStepLen = matrix.size() / 4;
+        int rowStepLen = matrix.get(0).size() / 4;
+        int countRow = 0;
+        for (int i = 0; i + colStepLen <= matrix.size(); i += colStepLen) {
+            int countCol = 0;
+            for (int j = 0; j + rowStepLen <= matrix.get(0).size(); j += rowStepLen) {
+                List<List<Long>> singleMat = new ArrayList<>();
+                //every row
+                for (int k = i; k < i + colStepLen; k++) {
+                    List<Long> singleRow = new ArrayList<>();
+                    //every col
+                    for (int m = j; m < j + rowStepLen; m++) {
+                        singleRow.add(matrix.get(k).get(m));
+                    }
+                    singleMat.add(singleRow);
+                }
+                SplitedMat splitedMat = new SplitedMat(countRow, countCol, singleMat);
+                ans.add(splitedMat);
+                countCol++;
+            }
+            countRow++;
+        }
+        return ans;
+    }
 
-        zg.putAnError(newMat);
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input your selection, 1 is version1, 2 is version2(distributed version): ");
+        int select = sc.nextInt();
+        if (select == 1) {
+            String filePath1 = "/Users/zuoankembp/Desktop/CSE8377/zGemm/matrix1.csv";
+            String filePath2 = "/Users/zuoankembp/Desktop/CSE8377/zGemm/matrix2.csv";
+            zGemm zg = new zGemm();
+            //get matrix
+            List<List<Long>> mat1 = zg.readMat(filePath1);
+            List<List<Long>> mat2 = zg.readMat(filePath2);
 
-        System.out.println("**************************************************");
-        System.out.println("Matrix multiple finished! Has inject one ERROR!");
-        System.out.println("Error detecting started!");
-        System.out.println("**************************************************");
+            long cStart = System.currentTimeMillis();
+            //matrix mul
+            List<List<Long>> newMat = zg.matMuli(mat1, mat2);
+            long cEnd = System.currentTimeMillis();
 
-        long dStart = System.currentTimeMillis();
+            zg.putAnError(newMat);
 
-        //get the origin check sum list
-        List<Long> originColCheckSumList = zg.getColCheckSum(mat1);
-        List<Long> originRowCheckSumList = zg.getRowCheckSum(mat2);
-        //cal the correct check sum list
-        List<Long> colCheckSumShouldBe = zg.calColCheckSum(originColCheckSumList, mat2);
-        List<Long> rowCheckSumShouldBe = zg.calRowCheckSum(mat1, originRowCheckSumList);
+            System.out.println("**************************************************");
+            System.out.println("Matrix multiple finished! Has inject one ERROR!");
+            System.out.println("Error detecting started!");
+            System.out.println("**************************************************");
 
-        //get the new check sum list
-        List<Long> newColCheckSum = zg.getColCheckSum(newMat);
-        List<Long> newRowCheckSum = zg.getRowCheckSum(newMat);
+            long dStart = System.currentTimeMillis();
 
-        List<Integer> errorPos = zg.detectError(colCheckSumShouldBe, newColCheckSum, rowCheckSumShouldBe, newRowCheckSum);
+            //get the origin check sum list
+            List<Long> originColCheckSumList = zg.getColCheckSum(mat1);
+            List<Long> originRowCheckSumList = zg.getRowCheckSum(mat2);
+            //cal the correct check sum list
+            List<Long> colCheckSumShouldBe = zg.calColCheckSum(originColCheckSumList, mat2);
+            List<Long> rowCheckSumShouldBe = zg.calRowCheckSum(mat1, originRowCheckSumList);
 
-        long dEnd = System.currentTimeMillis();
+            //get the new check sum list
+            List<Long> newColCheckSum = zg.getColCheckSum(newMat);
+            List<Long> newRowCheckSum = zg.getRowCheckSum(newMat);
 
-        System.out.println("Error detected, position: Row: " + errorPos.get(0) + " Col: " + errorPos.get(1));
-        System.out.println("Matrix multiple time: " + (cEnd - cStart) + " ms");
-        System.out.println("Error detection time: " + (dEnd - dStart) + " ms");
+            List<Integer> errorPos = zg.detectError(colCheckSumShouldBe, newColCheckSum, rowCheckSumShouldBe, newRowCheckSum);
+
+            long dEnd = System.currentTimeMillis();
+
+            System.out.println("Error detected, position: Row: " + errorPos.get(0) + " Col: " + errorPos.get(1));
+            System.out.println("Matrix multiple time: " + (cEnd - cStart) + " ms");
+            System.out.println("Error detection time: " + (dEnd - dStart) + " ms");
+        } else if (select == 2) {
+            String filePath1 = "/Users/zuoankembp/Desktop/CSE8377/zGemm/matrix1.csv";
+            String filePath2 = "/Users/zuoankembp/Desktop/CSE8377/zGemm/matrix2.csv";
+            zGemm zg = new zGemm();
+            //get matrix
+            List<List<Long>> mat1data = zg.readMat(filePath1);
+            List<List<Long>> mat2data = zg.readMat(filePath2);
+            int[] temp = new int[]{0, 0};
+        } else if (select == 3) {
+            String filePath1 = "/Users/zuoankembp/Desktop/CSE8377/zGemm/matrix1.csv";
+            String filePath2 = "/Users/zuoankembp/Desktop/CSE8377/zGemm/matrix2.csv";
+            zGemm zg = new zGemm();
+            //get matrix
+            List<List<Long>> mat1 = zg.readMat(filePath1);
+            List<List<Long>> mat2 = zg.readMat(filePath2);
+            zg.splitMatrix(mat1);
+        }
+    }
+
+    static class Matrix implements Runnable {
+        List<List<Long>> mat1;
+        List<List<Long>> mat2;
+        List<List<Long>> mat3;
+
+        List<Integer> pos;
+        Matrix(List<List<Long>> mat1, List<List<Long>> mat2, List<Integer> pos) {
+            this.mat1 = mat1;
+            this.mat2 = mat2;
+            this.pos = pos;
+        }
+
+        public List<List<Long>> matMuli(List<List<Long>> mat1, List<List<Long>> mat2) {
+            System.out.println("Matrix multiple start!");
+            List<List<Long>> res = new ArrayList<>();
+            int count = 0;
+            for (List<Long> mat1row : mat1) {
+                List<Long> newRow = new ArrayList<>();
+                for (int k = 0; k < mat2.get(0).size(); k++) {
+                    long sum = 0;
+                    for (int i = 0, j = 0; i < mat1row.size() && j < mat2.size(); i++, j++) {
+                        sum += mat1row.get(i) * (mat2.get(j).get(k));
+                    }
+                    newRow.add(sum);
+                    count++;
+                    if (count % 10485 == 0) {
+                        System.out.println("%" + count / 10485);
+                    }
+                }
+                res.add(newRow);
+            }
+            return res;
+        }
+
+        @Override
+        public void run() {
+            this.mat3 = matMuli(this.mat1, this.mat2);
+        }
     }
 
 }
